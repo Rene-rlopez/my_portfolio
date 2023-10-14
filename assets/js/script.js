721e0cc324eda2972d555f4939dd1879e74fb7a8
Rene Lopez <132025382+Rene-rlopez@users.noreply.github.com>
2023-10-14 17:33:58 -0600

    Navbar updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const navLink = document.querySelectorAll('.linkMenu');
 const sections = document.querySelectorAll('.section');
 
+document.querySelector(".leftBar .toggle-btn").addEventListener("click", function(){
+    document.querySelector(".leftBar").classList.toggle("active");
+});
+
+
 let currentSection = 'home';
 window.addEventListener('scroll', () => {
     sections.forEach(section => {
